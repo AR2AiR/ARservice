ba34feb7ed8a7371d282380e2a2d23f8ce69dd8a
Marcel Malik <marcel@einmalik.eu>
2019-10-20 09:25:48 +0200

    add date and time handling to copernicus image forecast

--- src/mvc/copernicus_controller.go
+++ src/mvc/copernicus_controller.go
@@ -6,12 +6,15 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"time"
 )
 
 func loadCopernicusImageForecast(w http.ResponseWriter, r *http.Request) {
 
 	service := getParamString(r, "service", "ENSEMBLE-FORECAST")
 	species := getParamString(r, "species", "PM10")
+	date := getParamInt(r, "date", 0)
+	hour := getParamInt(r, "time", 0) // valid values 0..23
 	startLatitude := getParamFloat64(r, "startLatitude", 30.05)
 	endLatitude := getParamFloat64(r, "endLatitude", -24.95)
 	startLongitude := getParamFloat64(r, "startLongitude", 71.95)
@@ -20,7 +23,7 @@ func loadCopernicusImageForecast(w http.ResponseWriter, r *http.Request) {
 	width := getParamInt(r, "width", 810)
 	height := getParamInt(r, "height", 495)
 
-	response := queryCopernicusImageData(service, species, startLatitude, endLatitude, startLongitude, endLongitude, level, width, height)
+	response := queryCopernicusImageForecast(service, species, date, hour, startLatitude, endLatitude, startLongitude, endLongitude, level, width, height)
 	replyImageData(response, w)
 }
 
@@ -35,7 +38,7 @@ func replyImageData(imageData []byte, w http.ResponseWriter) {
 	}
 }
 
-func queryCopernicusImageData(service string, species string, startLatitude float64, endLatitude float64, startLongitude float64, endLongitude float64, level int64, width int64, height int64) []byte {
+func queryCopernicusImageForecast(service string, species string, date int, hour int,  startLatitude float64, endLatitude float64, startLongitude float64, endLongitude float64, level int, width int, height int) []byte {
 
 	const apiToken = "__-Tlb4srqXM2EzZkgj_Va5OdozWR9G-dW7TS0yFsZL0I__"
 	serviceURL := fmt.Sprintf("https://geoservices.regional.atmosphere.copernicus.eu/services/CAMS50-%s-01-EUROPE-WMS", service)
@@ -45,13 +48,17 @@ func queryCopernicusImageData(service string, species string, startLatitude floa
 		log.Fatal("Malformed URL: ", err.Error())
 	}
 
-	params := url.Values {}
+	filterDate := time.Now().UTC().AddDate(0, 0, -date).Format("2006-01-02")
+
+	params := url.Values{}
 	params.Add("service", "WMS")
 	params.Add("VERSION", "1.3.0")
 	params.Add("LAYERS", fmt.Sprintf("%s__SPECIFIC_HEIGHT_LEVEL_ABOVE_GROUND", species))
 	params.Add("STYLES", fmt.Sprintf("%s_USI__HEIGHT__SHADING", species))
-	params.Add("DIM_REFERENCE_TIME", "2019-10-17T00:00:00Z")
-	params.Add("TIME", "2019-10-17T05:00:00Z")
+	// params.Add("DIM_REFERENCE_TIME", "2019-10-17T00:00:00Z")
+	params.Add("DIM_REFERENCE_TIME", fmt.Sprintf("%sT00:00:00Z", filterDate))
+	// params.Add("TIME", "2019-10-17T05:00:00Z")
+	params.Add("TIME", fmt.Sprintf("%sT%02d:00:00Z", filterDate, hour))
 	params.Add("ELEVATION", fmt.Sprintf("%d", level))
 	params.Add("FORMAT", "image/png")
 	params.Add("TOKEN", apiToken)
@@ -68,7 +75,7 @@ func queryCopernicusImageData(service string, species string, startLatitude floa
 	baseURL.RawQuery = url
 
 	// log.Printf("query %s", url)
-	log.Printf("url %s", baseURL.String())
+	// log.Printf("url %s", baseURL.String())
 
 	response, err := http.Get(baseURL.String())
 	if err != nil {
@@ -86,7 +93,7 @@ func queryCopernicusImageData(service string, species string, startLatitude floa
 func queryCopernicusImageDataV1() []byte {
 
 	const apiToken = "__-Tlb4srqXM2EzZkgj_Va5OdozWR9G-dW7TS0yFsZL0I__"
-	const serviceURL = "https://geoservices.regional.atmosphere.copernicus.eu/services/CAMS50-ENSEMBLE-FORECAST-01-EUROPE-WMS?&service=WMS&VERSION=1.3.0&LAYERS=PM10__SPECIFIC_HEIGHT_LEVEL_ABOVE_GROUND&STYLES=PM10_USI__HEIGHT__SHADING&DIM_REFERENCE_TIME=2019-10-17T00:00:00Z&TIME=2019-10-17T05:00:00Z&ELEVATION=0&FORMAT=image/png&TOKEN=__-Tlb4srqXM2EzZkgj_Va5OdozWR9G-dW7TS0yFsZL0I__&REQUEST=GetMap&CRS=EPSG:4326&BBOX=30.05,-24.95,71.95,44.95&WIDTH=810&HEIGHT=495";
+	const serviceURL = "https://geoservices.regional.atmosphere.copernicus.eu/services/CAMS50-ENSEMBLE-FORECAST-01-EUROPE-WMS?&service=WMS&VERSION=1.3.0&LAYERS=PM10__SPECIFIC_HEIGHT_LEVEL_ABOVE_GROUND&STYLES=PM10_USI__HEIGHT__SHADING&DIM_REFERENCE_TIME=2019-10-17T00:00:00Z&TIME=2019-10-17T05:00:00Z&ELEVATION=0&FORMAT=image/png&TOKEN=__-Tlb4srqXM2EzZkgj_Va5OdozWR9G-dW7TS0yFsZL0I__&REQUEST=GetMap&CRS=EPSG:4326&BBOX=30.05,-24.95,71.95,44.95&WIDTH=810&HEIGHT=495"
 
 	response, err := http.Get(serviceURL)
 	if err != nil {
--- src/mvc/http_request_helper.go
+++ src/mvc/http_request_helper.go
@@ -23,11 +23,11 @@ func getParamFloat64(r *http.Request, name string, defaultValue float64) float64
 	return value
 }
 
-func getParamInt(r *http.Request, name string, defaultValue int64) int64 {
+func getParamInt(r *http.Request, name string, defaultValue int) int {
 	queryValue := r.URL.Query().Get(name)
 	if (queryValue == "") {
 		return defaultValue
 	} 
-	value, _ := strconv.ParseInt(queryValue, 10, 64)
-	return value
+	value, _ := strconv.ParseInt(queryValue, 10, 32)
+	return int(value)
 }
